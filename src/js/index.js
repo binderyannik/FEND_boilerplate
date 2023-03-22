071fda784a6f315e57732c97942e76d37359085a
Yannik Binder <yannikbinder@gmail.com>
2023-03-22 20:54:50 +0100

    Eric hat am Code gefuscht

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,36 @@
-import myFunction, { multiply } from "./myModule";
+/*import myFunction, { multiply } from "./myModule";
 
 myFunction();
 
 const result = multiply(2, 5);
 
-console.log(result);
+console.log(result);*/
+
+
+const productContainer = document.querySelector(".product-container");
+
+let productsHtml = "";
+
+const products = [
+    {
+        name: "Brasilien",
+        price: 1000,
+    },
+    {
+        name: "Ecuador",
+        price: 10000,
+    }
+];
+
+products.forEach((product) => {
+    console.log(product);
+    const productHtml = `
+        <div class="product">
+            <div class="product-title">${product.name}</div>
+        </div>
+    `;
+    productsHtml = productsHtml + productHtml;
+})
+
+
+productContainer.innerHTML = productsHtml;
